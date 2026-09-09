15ccdc5601875452df4279b40e3b39e196ebfb56
hanota <hanota94@gmail.com>
2026-09-09 04:33:29 +0100

    commit 3

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/https://github.com/hanota94-source/Portofolio2/",
+  base: "/Portofolio/",
 });
